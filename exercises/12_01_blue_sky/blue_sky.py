5abdd0d79352ffd803567d0aad53a377fc677b84
xerifeazeitona <juliano.amaral@gmail.com>
2021-01-29 16:37:12 -0500

    Added exercise 12-1, Blue Sky

---
+++ exercises/12_01_blue_sky/blue_sky.py
@@ -0,0 +1,25 @@
+"""
+12-1. Blue Sky: Make a Pygame window with a blue background.
+"""
+import sys
+import pygame
+
+# initialize pygame
+pygame.init()
+# create surface
+surface = pygame.display.set_mode((1200, 800))
+# set title bar caption
+pygame.display.set_caption("Blue Sky")
+# blue sky
+bg_color = (135, 206, 235)
+# set background color to sky blue
+surface.fill(bg_color)
+
+while True:
+    # loops until window is closed
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
+
+    # Make the most recently drawn screen visible
+    pygame.display.flip()
