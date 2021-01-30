6e094cf97023b0045a90b60ca0c103d6e27bb910
xerifeazeitona <juliano.amaral@gmail.com>
2021-01-30 16:10:50 -0500

    added fullscreen mode

--- game/alien_invasion.py
+++ game/alien_invasion.py
@@ -17,8 +17,7 @@ class AlienInvasion:
         self.settings = Settings()
 
         # Create a display window and assign to self.screen
-        self.screen = pygame.display.set_mode(
-            (self.settings.screen_width, self.settings.screen_height))
+        self.screen = self.set_screen_mode()
         pygame.display.set_caption("Alien Invasion")
 
         self.ship = Ship(self)
@@ -37,15 +36,38 @@ class AlienInvasion:
             if event.type == pygame.QUIT:
                 sys.exit()
             elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_RIGHT:
-                    self.ship.moving_right = True
-                elif event.key == pygame.K_LEFT:
-                    self.ship.moving_left = True
+                self._check_keydown_events(event)
             elif event.type == pygame.KEYUP:
-                if event.key == pygame.K_RIGHT:
-                    self.ship.moving_right = False
-                elif event.key == pygame.K_LEFT:
-                    self.ship.moving_left = False           
+                self._check_keyup_events(event)
+
+    def _check_keydown_events(self, event):
+        """Respond to keypresses."""
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = True
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = True
+        elif event.key == pygame.K_q:
+            sys.exit()
+
+    def _check_keyup_events(self, event):
+        """Respond to keypresses."""
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = False
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = False
+
+    def set_screen_mode(self):
+        """Toggle between fullscreen and windowed."""
+        if self.settings.fullscreen:
+            surface = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+            self.settings.screen_width = surface.get_rect().width
+            self.settings.screen_height = surface.get_rect().height
+            self.settings.fullscreen = False
+        else:
+            surface = pygame.display.set_mode(
+                (self.settings.screen_width, self.settings.screen_height))
+            self.settings.fullscreen = True
+        return surface
 
     def _update_screen(self):
         """Update images on the screen, and flip to the new screen."""
