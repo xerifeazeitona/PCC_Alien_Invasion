ebc96502404ea896bb93651065642dbeed78543b
xerifeazeitona <juliano.amaral@gmail.com>
2021-01-29 21:07:19 -0500

    Initial commit

---
+++ game/settings.py
@@ -0,0 +1,14 @@
+"""
+Instead of adding settings throughout the code, we'll store all these
+values in a class.
+"""
+class Settings:
+    """A class to store all settings for Alien Invasion."""
+
+    def __init__(self):
+        """Initialize the game's settings."""
+        # Screen settings
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (230, 230, 230)
+        
