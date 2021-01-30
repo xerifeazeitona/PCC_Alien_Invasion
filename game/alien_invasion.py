1aa67c137f0249ad3e0fba3bef814e89b3e3254b
xerifeazeitona <juliano.amaral@gmail.com>
2021-01-30 15:13:09 -0500

    Added movement to the right

--- game/alien_invasion.py
+++ game/alien_invasion.py
@@ -28,6 +28,7 @@ class AlienInvasion:
         while True:
             # Watch for keyboard and mouse events.
             self._check_events()
+            self.ship.update()
             self._update_screen()
 
     def _check_events(self):
@@ -35,6 +36,12 @@ class AlienInvasion:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RIGHT:
+                    self.ship.moving_right = True
+            elif event.type == pygame.KEYUP:
+                if event.key == pygame.K_RIGHT:
+                    self.ship.moving_right = False                    
 
     def _update_screen(self):
         """Update images on the screen, and flip to the new screen."""
