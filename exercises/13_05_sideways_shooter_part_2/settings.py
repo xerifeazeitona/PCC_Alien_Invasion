7cd7ab3229d8f91e76a0a5189c92b42c0f670958
xerifeazeitona <juliano.amaral@gmail.com>
2021-02-03 12:30:11 -0500

    added exercise 13-5

---
+++ exercises/13_05_sideways_shooter_part_2/settings.py
@@ -0,0 +1,28 @@
+"""
+Settings class for Super Rocket
+"""
+class Settings:
+    "A class to store all settings for Super Rocket."
+
+    def __init__(self):
+        """Initialize the game's settings."""
+        # Screen settings
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (10, 20, 50)
+
+        # Rocket settings
+        self.rocket_speed = 1.5
+
+        # Bullet settings
+        self.bullet_speed = 1.0
+        self.bullet_width = 15
+        self.bullet_height = 3
+        self.bullet_color = (120, 120, 120)
+        self.bullets_allowed = 3
+
+        # Enemy settings
+        self.enemy_speed = 1.0
+        self.fleet_advance_speed = 10
+        # 1 for up, -1 for down
+        self.fleet_direction = 1
